285cb2089c2c325b3c2a835c913f1b897329ce65
MuhammadZahabAhmad <zabiirana27@gmail.com>
2026-06-14 19:08:20 +0500

    Remove plugin management from build-logic/settings.gradle.kts

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,16 +1,3 @@
-pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal() // THIS IS THE REQUIRED LINE
-    }
-}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
