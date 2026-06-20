954de7e7d601aa416fa5666f9e4238b65fe1dcd0
MuhammadZahabAhmad <zabiirana27@gmail.com>
2026-06-21 01:40:55 +0500

    Implement String resource management

--- core/domain/src/main/java/com/zahab/core/domain/util/DataError.kt
+++ core/domain/src/main/java/com/zahab/core/domain/util/DataError.kt
@@ -1,12 +1,20 @@
 package com.zahab.core.domain.util
 
-interface DataError: Error
-enum class NetworkError : DataError {
-    SERVER_ERROR,
-    TOO_MANY_REQUESTS,
-    TIME_OUT
-}
 
-enum class LocalErrors : DataError{
-    DISK_FULL
+sealed interface DataError: Error {
+    enum class Network: DataError {
+        REQUEST_TIMEOUT,
+        UNAUTHORIZED,
+        CONFLICT,
+        TOO_MANY_REQUESTS,
+        NO_INTERNET,
+        PAYLOAD_TOO_LARGE,
+        SERVER_ERROR,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local: DataError {
+        DISK_FULL
+    }
 }
